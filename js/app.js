f136994b7626e02bfe1445266e905c2f2ba15dc6
Ibrahim Ramirez <ibrahimramirez085@gmail.com>
2021-09-13 08:03:16 -0500

    version 0.0.1

---
+++ js/app.js
@@ -0,0 +1,28 @@
+const templateCard = document.getElementById('template-cont').content;
+const card = document.getElementById('main-container');
+const fragment = document.createDocumentFragment();
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+});
+
+const fetchData = async () => {
+    try {
+        const res = await fetch('../api/hardware.json');
+        const data = await res.json();
+        drawComponent(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const drawComponent = (data) => {
+    data.forEach(element => {
+        templateCard.querySelector('h2').textContent = element.name;
+        templateCard.querySelector('p').textContent = element.content;
+        templateCard.querySelector('img').setAttribute('src', element.pic);
+        const clone = templateCard.cloneNode(true);
+        fragment.appendChild(clone);
+    });
+    card.appendChild(fragment);
+}
